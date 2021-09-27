93950aa737251af927b49c7fe5f12bdf2110f7bd
1van0v <ivanov5oleksandr@gmail.com>
2021-09-27 20:19:55 +0300

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://5a93nu96k4.execute-api.eu-west-1.amazonaws.com/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8nwamel3th.execute-api.eu-west-1.amazonaws.com',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
