2f7c75f8a1b9cf14ea267781806f97a6a6535a61
1van0v <ivanov5oleksandr@gmail.com>
2021-10-21 03:04:37 +0300

    feat: add token to import request

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
+import { Alert, AlertTitle } from '@material-ui/lab';
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   content: {
     padding: theme.spacing(3, 0, 3),
   },
+  alert: {
+    top: '80px',
+    right: '10px',
+    width: '300px',
+    position: 'fixed'
+  }
 }));
 
 type CSVFileImportProps = {
@@ -17,6 +24,7 @@ type CSVFileImportProps = {
 export default function CSVFileImport({url, title}: CSVFileImportProps) {
   const classes = useStyles();
   const [file, setFile] = useState<any>();
+  const [alert, setAlert] = useState<string>();
 
   const onFileChange = (e: any) => {
     console.log(e);
@@ -31,13 +39,30 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
-      const response = await axios({
-        method: 'GET',
-        url,
-        params: {
-          name: encodeURIComponent(file.name)
+      const token = localStorage.getItem('token');
+      let response;
+
+      try {
+        const headers = token ? {Authorization: `Basic ${token}`} : {};
+        response = await axios({
+          method: 'GET',
+          url,
+          params: {
+            name: encodeURIComponent(file.name)
+          },
+          headers
+        });
+      } catch (e) {
+        if ([401, 403].includes(e.status)) {
+          console.log(e);
+          setAlert(`Action is ${e.data.message}`);
+          setTimeout(() => setAlert(''), 10000);
+          return;
         }
-      })
+
+        throw(e);
+      }
+
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
       const result = await fetch(response.data, {
@@ -62,6 +87,13 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
           <button onClick={uploadFile}>Upload file</button>
         </div>
       )}
+
+      {alert &&
+        <Alert variant="outlined" severity="error" className={classes.alert}>
+          <AlertTitle>Error</AlertTitle>
+          {alert}
+        </Alert>
+      }
     </div>
   );
 }
